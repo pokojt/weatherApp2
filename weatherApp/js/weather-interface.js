fc4481524a2c80e23ad505b7c9e588fe9d7b209f
Taylor Pokoj <pokojt@gmail.com>
2016-03-15 15:07:42 -0700

    Update weather-interface script file to start from beginning

--- weatherApp/js/weather-interface.js
+++ weatherApp/js/weather-interface.js
@@ -1,18 +1,15 @@
-// var apiKey = "YOUR-API-KEY-GOES-HERE";
-// var apiKey = require('./../.env').apiKey;
-//
-// $(document).ready(function() {
-//   $('#weatherLocation').click(function() {
-//     var city = $('#location').val();
-//     $('#location').val("");
-//     $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-//     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-//      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-//    }).fail(function(error) {
-//      $('.showWeather').text(error.message);
-//    });
-//  });
-// });
+
+var apiKey = require('./../.env').apiKey;
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
+  });
+});
+
+
 
 
 
