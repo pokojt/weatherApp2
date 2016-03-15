7496325ac57a6e3cf16e9d2aa2ba58f734c883e6
Hillary Hintzen and Taylor Pokoj <hillary07@gmail.com>
2016-03-15 14:07:45 -0700

    Add functionality to alarm alert

--- alarmClock/js/alarm.js
+++ alarmClock/js/alarm.js
@@ -1,19 +1,15 @@
 
-exports.Clock = function(alarmTime) {
-  this.alarmTime = alarmTime;
+exports.Clock = function() {
+  // this.alarmTime = alarmTime;
 };
 
 exports.Clock.prototype.update = function() {
   $(".currentTime").text(moment().format('LTS'));
+};
 
-    var currentTime = moment().format('LTS');
+exports.Clock.prototype.check = function() {
 
-    return this.alarmTime;
-
-    console.log(this.alarmTime);
-
-    if(alarmTime === currentTime) {
-      // alert('ALARM!!!!');
+    if(this.alarmTime === moment().format('HH:mm')) {
       $('body').addClass("alert");
     };
 };
--- alarmClock/js/browser-interface.js
+++ alarmClock/js/browser-interface.js
@@ -2,19 +2,24 @@ var Alarm = require('../js/alarm.js').Clock;
 // var hi = require('../js/alarm.js').greeting
 
 $(document).ready(function (){
-  var newAlarm = new Alarm(inputtedAlarmTime);
-  var inputtedAlarmTime = $("input#userAlarmTime").val();
+
+  var newAlarm = new Alarm();
 
   setInterval(function() {
     newAlarm.update();
+    newAlarm.check();
   }, 1000);
 
+console.log(newAlarm);
 
   $(".alarm").submit(function() {
     event.preventDefault();
     var inputtedAlarmTime = $("input#userAlarmTime").val();
 
-    $(".alarmConfirm").append("<h4>Your alarm is set for " + inputtedAlarmTime + ".</h4>");
+    newAlarm.alarmTime = inputtedAlarmTime;
+    console.log(newAlarm);
+
+    $(".alarmConfirm").append("<h4 time="+ inputtedAlarmTime +">Your alarm is set for " + inputtedAlarmTime + ".</h4>");
 
   });
 
