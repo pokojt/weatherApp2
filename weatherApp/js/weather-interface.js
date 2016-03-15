2b50c9e8df8db29ddb30a58dd8abb97bcce2abb6
Taylor Pokoj <pokojt@gmail.com>
2016-03-15 15:14:27 -0700

    Add functionality to API

--- weatherApp/js/weather-interface.js
+++ weatherApp/js/weather-interface.js
@@ -5,7 +5,9 @@ $(document).ready(function() {
   $('#weatherLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%. DAMN.");
+    });
   });
 });
 
